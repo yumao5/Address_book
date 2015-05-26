38c5a44c917c362e16d2875bdcd8cc2bfbc219c3
yumao.ca@gmail.com <lens55mm@gmail.com>
2015-05-25 22:37:27 -0400

    11th

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe AdminUser, type: :model do
 
+     it "Model valid check" do
+            user = AdminUser.new(
+              first_name: 'Aaron',
+              last_name: 'Sumner',
+              email: 'tester@example.com')
+            expect(user).to be_valid
+     end
+
+     it "Model valid check" do
+            user = AdminUser.new(
+              first_name: 'AaronSumnerSumnerSumnerSumnerSumnerSumnerSumnerSumner',
+              last_name: 'Sumner',
+              email: 'tester@example.com')
+            expect(user).to be_invalid
+     end
+
 end

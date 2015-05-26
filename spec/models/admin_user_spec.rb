b23a20a527fa444dd07cc05ac94f52d0e430cf5f
yumao.ca@gmail.com <lens55mm@gmail.com>
2015-05-25 23:06:35 -0400

    11th

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -2,20 +2,29 @@ require 'rails_helper'
 
 RSpec.describe AdminUser, type: :model do
 
-     it "Model valid check" do
-            user = AdminUser.new(
-              first_name: 'Aaron',
-              last_name: 'Sumner',
-              email: 'tester@example.com')
-            expect(user).to be_valid
+     it "@ Model valid check" do
+          user = AdminUser.new(
+            first_name: 'Aaron',
+            last_name: 'Sumner',
+            email: 'tester@example.com')
+          expect(user).to be_valid
      end
 
-     it "Model valid check" do
-            user = AdminUser.new(
-              first_name: 'AaronSumnerSumnerSumnerSumnerSumnerSumnerSumnerSumner',
-              last_name: 'Sumner',
-              email: 'tester@example.com')
-            expect(user).to be_invalid
+     it "@ Model valid check" do
+          user = AdminUser.new(
+            first_name: 'AaronSumnerSumnerSumnerSumnerSumnerSumnerSumnerSumner',
+            last_name: 'Sumner',
+            email: 'tester@example.com')
+          expect(user).to be_invalid
+     end
+
+     it "@ Model instance mothod check" do
+       user = AdminUser.new(
+         first_name: 'Aaron',
+         last_name: 'Sumner',
+         email: 'tester@example.com')
+
+       expect(user.name).to eq 'Aaron Sumner'
      end
 
 end

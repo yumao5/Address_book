4938a390a3ea85274ee3c471d1d64096e5c7b59d
yumao.ca@gmail.com <lens55mm@gmail.com>
2015-05-28 20:29:56 -0400

    setup file adjustment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 SimpleCms::Application.routes.draw do
 
   root "access#index"
-  
+
+  # match 'search' => 'admin_users#search', :via => [:get, :post]
   get 'get_contactlists' => 'admin_users#api_show_contacts', :as => 'get_contactlists'
   get 'del_contact' => 'admin_users#api_dele_contact', :as => 'del_contact'
   post 'update_contact' => 'admin_users#api_update_contact', :as => 'update_contact', :via => [:post]

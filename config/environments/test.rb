22c1fe2a6261e5f41210521255011139dc25b76c
yumao.ca@gmail.com <lens55mm@gmail.com>
2015-05-28 20:31:45 -0400

    setup file adjustment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,4 @@
-SimpleCms::Application.configure do
+Addressbook::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
